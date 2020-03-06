dea83fb8b6023fb6d60b9c8e118e5c72fb373e62
iamz-tao <phiyada.s@ku.th>
2020-03-06 17:34:10 +0700

    Edit student home page

--- src/pages/student/index.js
+++ src/pages/student/index.js
@@ -3,7 +3,16 @@ import styled from 'styled-components'
 import { createStructuredSelector } from 'reselect'
 import { bindActionCreators, compose } from 'redux'
 import { connect } from 'react-redux'
-import { Modal, notification } from 'antd'
+import { Modal, notification, Menu, Button } from 'antd'
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  InboxOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
 import Router from 'next/router'
 
 import Cookie from 'js-cookie'
@@ -18,6 +27,7 @@ import { userAction } from '~/modules/admin/actions'
 import { userSelector } from '~/modules/admin/selectors'
 
 const { confirm } = Modal
+const { SubMenu } = Menu
 
 const TableHeader = () => (
   <Wrapper>
@@ -68,6 +78,7 @@ class StudentHomePage extends Component {
       user_role: [],
       keyword: '',
     },
+    collapsed: false,
   }
 
   componentDidMount() {
@@ -155,6 +166,12 @@ class StudentHomePage extends Component {
     })
   }
 
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
     const {
       users,
@@ -168,6 +185,61 @@ class StudentHomePage extends Component {
       <PageWrapper>
         <RowContainer>
           <RowContainer style={{ paddingTop: 0 }}>
+          <div style={{ width: 256 }}>
+        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+          {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+        </Button>
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={this.state.collapsed}
+        >
+          <Menu.Item key="1">
+            <PieChartOutlined />
+            <span>Option 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <DesktopOutlined />
+            <span>Option 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <InboxOutlined />
+            <span>Option 3</span>
+          </Menu.Item>
+          <SubMenu
+            key="sub1"
+            title={
+              <span>
+                <MailOutlined />
+                <span>Navigation One</span>
+              </span>
+            }
+          >
+            <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            title={
+              <span>
+                <AppstoreOutlined />
+                <span>Navigation Two</span>
+              </span>
+            }
+          >
+            <Menu.Item key="9">Option 9</Menu.Item>
+            <Menu.Item key="10">Option 10</Menu.Item>
+            <SubMenu key="sub3" title="Submenu">
+              <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item>
+            </SubMenu>
+          </SubMenu>
+        </Menu>
+      </div>
             <ListCol
               style={{
                 position: 'relative',
